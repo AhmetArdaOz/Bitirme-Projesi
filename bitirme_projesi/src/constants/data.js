e45c9a3afdbc2c227e8062c3d2512598d2ccbcb8
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-03-09 17:17:17 +0300

    changes
    added some movies as cards and finished the roots

---
+++ bitirme_projesi/src/constants/data.js
@@ -0,0 +1,23 @@
+export const movieData =[
+{
+  id:1,
+  title:"Matrix",
+  genre:"Action",
+  imageUrl:"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+},
+{
+    id:2,
+    title:"Braveheart",
+    genre:"Action,History",
+    imageUrl:"https://m.media-amazon.com/images/M/MV5BMzkzMmU0YTYtOWM3My00YzBmLWI0YzctOGYyNTkwMWE5MTJkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+},
+
+{
+    id:3,
+    title:"Spiderman",
+    genre:"Superhero",
+    imageUrl:"https://upload.wikimedia.org/wikipedia/en/6/6c/Spider-Man_%282002_film%29_poster.jpg"
+},
+
+
+]
--- bitirme_projesi/src/pages/MainPage.js
+++ bitirme_projesi/src/pages/MainPage.js
@@ -12,6 +12,8 @@ import {
   ListItemText,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
+import { movieData } from "../constants/data"; // Importing movie data
 import "../styling/MainPage.css";
 
 export default function MainPage() {
@@ -29,43 +31,63 @@ export default function MainPage() {
 
   return (
       <div className="App">
-        <Container className="Container">
-          <AppBar className="AppBar" position="fixed">
-            <Toolbar className="AppBar-Toolbar">
-              <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  sx={{ mr: 2 }}
-                  onClick={toggleDrawer}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Project_Bitirme
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Drawer className="Drawer" anchor="left" open={drawerOpen} onClose={toggleDrawer}>
-            <List className="Drawer-List">
-              <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("/")}>
-                <ListItemText className="Drawer-ListItemText" primary="Home" />
-              </ListItem>
-              <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("movies")}>
-                <ListItemText className="Drawer-ListItemText" primary="Movies" />
-              </ListItem>
-              <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("series")}>
-                <ListItemText className="Drawer-ListItemText" primary="Series" />
-              </ListItem>
-              <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("oldbutgold")}>
-                <ListItemText className="Drawer-ListItemText" primary="Old but Gold" />
-              </ListItem>
-            </List>
-          </Drawer>
-        </Container>
-        <Container className="Container">
-          <Outlet />
+        <AppBar className="AppBar" position="fixed">
+          <Toolbar className="AppBar-Toolbar">
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={toggleDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Project_Bitirme
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer className="Drawer" anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+          <List className="Drawer-List">
+            <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("/")}>
+              <ListItemText className="Drawer-ListItemText" primary="Home" />
+            </ListItem>
+            <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("movies")}>
+              <ListItemText className="Drawer-ListItemText" primary="Movies" />
+            </ListItem>
+            <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("series")}>
+              <ListItemText className="Drawer-ListItemText" primary="Series" />
+            </ListItem>
+            <ListItem className="Drawer-ListItem" button onClick={() => handleDrawerItemClick("oldbutgold")}>
+              <ListItemText className="Drawer-ListItemText" primary="Old but Gold" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Container className="MainContent">
+          <div className="MovieList">
+            {movieData.map((movie) => (
+                <Card key={movie.id} className="MovieCard">
+                  <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="500"
+                        image={movie.imageUrl}
+                        alt={movie.title}
+                        className="MovieCard-Image"
+                    />
+                    <CardContent className="MovieCard-Content">
+                      <Typography gutterBottom variant="h5" component="div" className="MovieCard-Title">
+                        {movie.title}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" className="MovieCard-Genre">
+                        Genre: {movie.genre}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+            ))}
+          </div>
         </Container>
       </div>
   );
