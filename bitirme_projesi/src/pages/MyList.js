cb06f3a7031be85bafd646abcfd8d3f4c05a9013
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-03-10 14:02:30 +0300

    new page and some style to main page added

---
+++ bitirme_projesi/src/pages/MyList.js
@@ -0,0 +1,11 @@
+import "../styling/MyList.css"
+export default function MyList(){
+    return(
+        <div className="MyList">
+            <main>
+                MyList
+            </main>
+        </div>
+
+    )
+}
