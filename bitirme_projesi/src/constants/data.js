40f81604314458683c4fda05458cc6165aa02af4
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-03-10 18:18:33 +0300

    .

--- bitirme_projesi/src/constants/data.js
+++ bitirme_projesi/src/constants/data.js
@@ -1,23 +1,66 @@
 export const movieData =[
-{
-  id:1,
-  title:"Matrix",
-  genre:"Action",
-  imageUrl:"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
-},
-{
-    id:2,
-    title:"Braveheart",
-    genre:"Action,History",
-    imageUrl:"https://m.media-amazon.com/images/M/MV5BMzkzMmU0YTYtOWM3My00YzBmLWI0YzctOGYyNTkwMWE5MTJkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-},
+    {
+        id:1,
+        title:"Matrix",
+        genre:"Action",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+    },
+    {
+        id:2,
+        title:"Braveheart",
+        genre:"Action,History",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMzkzMmU0YTYtOWM3My00YzBmLWI0YzctOGYyNTkwMWE5MTJkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
 
-{
-    id:3,
-    title:"Spiderman",
-    genre:"Superhero",
-    imageUrl:"https://upload.wikimedia.org/wikipedia/en/6/6c/Spider-Man_%282002_film%29_poster.jpg"
-},
+    {
+        id:3,
+        title:"Spider-man",
+        genre:"Superhero",
+        imageUrl:"https://upload.wikimedia.org/wikipedia/en/6/6c/Spider-Man_%282002_film%29_poster.jpg"
+    },
+    {
+        id:4,
+        title:"Avengers: Endgame",
+        genre:"Superhero",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
+    },
+    {
+        id:5,
+        title:"Avatar The Last Airbender",
+        genre:"Child",
+        imageUrl:"https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p186109_b_v8_aa.jpg"
+    },
+    {
+        id:6,
+        title:"The Green Mile",
+        genre:"Drama",
+        imageUrl:"https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p24429_p_v12_bf.jpg"
+    },
+    {
+        id:7,
+        title:"Forrest Gump",
+        genre:"Drama",
+        imageUrl:"https://tr.web.img4.acsta.net/pictures/bzp/01/10568.jpg"
+    },
+    {
+        id:8,
+        title:"Arcane",
+        genre:"Adventure,Drama",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMWYxNGJhNmQtZmI4OS00NjQ1LThmNzAtY2JkYmQ5NjliNDgwXkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_.jpg"
+    },
+    {
+        id:9,
+        title:"SPIDER-MAN INTO THE SPIDERVERSE",
+        genre:"Superhero,Action",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_.jpg"
+    },
+    {
+        id:10,
+        title:"Inception",
+        genre:"Sci-fi,Action",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
+    },
+
 
 
 ]
--- bitirme_projesi/src/pages/MainPage.js
+++ bitirme_projesi/src/pages/MainPage.js
@@ -1,40 +1,58 @@
 import React from "react";
-import {
-  Typography,
-  Container,
-} from "@mui/material";
+import { Typography, Container } from "@mui/material";
 import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
 import { movieData } from "../constants/data";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../styling/MainPage.css";
 
 export default function MainPage() {
-  return (
-      <div className="App">
-        <Container className="MainContent">
-          <div className="MovieList">
-            {movieData.map((movie) => (
-                <Card key={movie.id} className="MovieCard">
-                  <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="500"
-                        image={movie.imageUrl}
-                        alt={movie.title}
-                        className="MovieCard-Image"
-                    />
-                    <CardContent className="MovieCard-Content">
-                      <Typography gutterBottom variant="h5" component="div" className="MovieCard-Title">
-                        {movie.title}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" className="MovieCard-Genre">
-                        Genre: {movie.genre}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-            ))}
-          </div>
-        </Container>
-      </div>
-  );
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    };
+
+    return (
+        <div className="App">
+            <Container className="MainContent">
+                <Slider {...settings}>
+                    {movieData.map((movie) => (
+                        <div key={movie.id} className="MovieCard">
+                            <Card className="MovieCard-Container">
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        image={movie.imageUrl}
+                                        alt={movie.title}
+                                        className="MovieCard-Image"
+                                    />
+                                    <CardContent className="MovieCard-Content">
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="div"
+                                            className="MovieCard-Title"
+                                        >
+                                            {movie.title}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="textSecondary"
+                                            className="MovieCard-Genre"
+                                        >
+                                            Genre: {movie.genre}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </div>
+                    ))}
+                </Slider>
+            </Container>
+        </div>
+    );
 }
