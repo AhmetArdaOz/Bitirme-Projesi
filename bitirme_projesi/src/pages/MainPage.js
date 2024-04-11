e4606eaf064431db4bed00c9b1e7d80c818d6278
Vlarice <kazimutkucitoglu@gmail.com>
2024-04-11 21:55:11 +0300

    .
    .

--- bitirme_projesi/src/pages/MainPage.js
+++ bitirme_projesi/src/pages/MainPage.js
@@ -56,6 +56,44 @@ export default function MainPage() {
                     ))}
                 </Slider>
             </Container>
+            <Container className="MainContent">
+                <Typography className="title" variant="h2" component="h2" style={{fontSize:"30px"}}>
+                    For You
+                </Typography>
+                <Slider {...settings}>
+                    {movieData.map((movie) => (
+                        <div key={movie.id} className="MovieCard">
+                            <Card className="MovieCard-Container">
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        image={movie.imageUrl}
+                                        alt={movie.title}
+                                        className="MovieCard-Image"
+                                    />
+                                    <CardContent className="MovieCard-Content">
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="div"
+                                            className="MovieCard-Title"
+                                        >
+                                            {movie.title}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="textSecondary"
+                                            className="MovieCard-Genre"
+                                        >
+                                            Genre: {movie.genre}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </div>
+                    ))}
+                </Slider>
+            </Container>
         </div>
     );
 }
