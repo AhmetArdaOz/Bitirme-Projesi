c960af483771a32d250ae5872d6b5804c0d838e2
Utku Çitoğlu <130839344+Vlarice@users.noreply.github.com>
2024-04-25 10:41:16 +0300

    .
    .

--- bitirme_projesi/src/pages/MainPage.js
+++ bitirme_projesi/src/pages/MainPage.js
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styling/MainPage.css";
 
-export default function MainPage() {aaa
+export default function MainPage() {
     const settings = {
         dots: true,
         infinite: true,
