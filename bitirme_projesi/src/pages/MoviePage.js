2bf4b37d72af5d2bfb6433d09f2012445f1ee5ef
Ahmet Arda Öz <70638681+AhmetArdaOz@users.noreply.github.com>
2024-05-08 10:01:57 +0300

    moviepage updated

--- bitirme_projesi/src/pages/MoviePage.js
+++ bitirme_projesi/src/pages/MoviePage.js
@@ -1,55 +1,93 @@
-import React from 'react';
-import { movieData } from '../constants/data';
-import '../styling/MoviePage.css';
+import React from "react";
+import { movieData } from "../constants/data";
+import "../styling/MoviePage.css";
 import { useParams } from "react-router-dom";
-import Rating from '@mui/material/Rating';
-import { Accordion,Typography } from '@mui/material';
+import Rating from "@mui/material/Rating";
+import { Accordion, Typography } from "@mui/material";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import AiPage from '../components/ChatAi';
+import AiPage from "../components/ChatAi";
 
 const MoviePage = () => {
-    const { id } = useParams();
+  const [value, setValue] = React.useState(0);
+  const { id } = useParams();
 
+  const movie = movieData.find((movie) => movie.id === parseInt(id));
 
-    const movie = movieData.find(movie => movie.id === parseInt(id));
+  if (!movie) {
+    console.log("Movie not found for ID:", id);
+    return <div>Movie not found</div>;
+  }
 
-    if (!movie) {
-        console.log("Movie not found for ID:", id);
-        return <div>Movie not found</div>;
-    }
+  const getRatingPhrase = (rating) => {
+    if (rating <= 1.5) return "Overwhelming dislike";
+    else if (rating <= 2.5) return "Generally unfavorable";
+    else if (rating <= 3.5) return "Generally favorable";
+    else return "Universal Acclaim";
+  };
 
-    console.log("Found Movie:", movie);
+  console.log("Found Movie:", movie);
 
-    return (
-        <>
-        <div className="movie-page-container">
-            <div className="trailer-container">
-                {/* Embedded YouTube trailer */}
-                <iframe
-                    className="trailer-video"
-                    src={movie.trailerUrl}
-                    title={movie.title}
-                    frameBorder="0"
-                    allowFullScreen
-                ></iframe>
-            </div>
-            <div className="info-container">
-                <h2 className="title">{movie.title}</h2>
-                <p className="director">Director: {movie.director}</p>
-                <p className="cast">Cast:{movie.cast}</p>
-                <p className="genre">Genre: {movie.genre}</p>
-                <p className="explanation">{movie.explanation}</p>
+  return (
+    <>
+      <div className="movie-page-container">
+        <div className="movie-page-rating">
+          <div className="trailer-container">
+            {/* picture */}
+            <iframe
+              className="trailer-video"
+              src={movie.trailerUrl}
+              title={movie.title}
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          {/* rating */}
+          <div className="rating-container">
+            <h2 className="title">{movie.title}</h2>
 
-                <Rating
-                    name="movie-rating"
-                    value={movie.rating || 0}
-                    readOnly
-                />
-            </div>
+            <p className="rating-general-title">METASCORE</p>
+            <p className="rating-general">{getRatingPhrase(movie.rating)}</p>
 
-            <div className="aipage">
+            <Rating
+              name="movie-rating-general"
+              value={movie.rating || 0}
+              precision={0.5}
+              readOnly
+            />
+
+            <p className="rating-user-title">USER SCORE</p>
+            <p className="rating-user">{getRatingPhrase(movie.rating)}</p>
+            <Rating
+              name="movie-rating-user"
+              value={movie.rating || 0}
+              precision={0.5}
+              readOnly
+            />
+
+            <p className="rating-personal-title">My Score</p>
+            <p className="rating-personal">{getRatingPhrase(value)}</p>
+            <Rating
+              name="movie-rating-personal"
+              value={value}
+              precision={0.5}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+          </div>
+        </div>
+        {/* info */}
+        <div className="info-container">
+          <p className="director">Director: {movie.director}</p>
+          <p className="cast">Cast:{movie.cast}</p>
+          <p className="genre">Genre: {movie.genre}</p>
+          <p className="explanation">{movie.explanation}</p>
+        </div>
+
+        {/* Ai chatbot */}
+        <div className="aipage">
           <Accordion sx={{ backgroundColor: "#1c1c1c", color: "white" }}>
             <AccordionSummary
               expandIcon={<ArrowDownwardIcon sx={{ color: "white" }} />}
@@ -57,17 +95,16 @@ const MoviePage = () => {
               id="panel1-header"
               sx={{ color: "white" }}
             >
-          <Typography>Talk to Famous Movie Stars</Typography>
+              <Typography>Talk to Famous Movie Stars</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <AiPage />
             </AccordionDetails>
           </Accordion>
         </div>
-            
-        </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default MoviePage;
