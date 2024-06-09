065cfe743558788eb6ff43801a36f232a25ab1e4
alpefesezer <alpefe.sezer@sesasis.com>
2024-06-09 22:11:50 +0300

    updateVote hatası düzeltildi

--- src/vote/queries.js
+++ src/vote/queries.js
@@ -1,19 +1,17 @@
 const getVotesByUserId = "SELECT * FROM votes WHERE user_id =$1";
-const getVotes = "SELECT * FROM votes"
+const getVotes = "SELECT * FROM votes";
 const addVote =
   "INSERT INTO votes (user_id, movie_id, vote) VALUES ($1, $2, $3)";
-const getVoteById = "SELECT * FROM votes WHERE id =$1"
+const getVoteById = "SELECT * FROM votes WHERE id =$1";
 const removeVote = "DELETE FROM votes WHERE id = $1";
-const updateUser =
+const updateVote =
   "UPDATE votes SET user_id = $1, movie_id = $2, vote = $3 WHERE id = $4";
 
-
 module.exports = {
-    getVotesByUserId,
-    getVotes,
-    addVote,
-    getVoteById,
-    removeVote,
-    updateUser,
-  };
-  
+  getVotesByUserId,
+  getVotes,
+  addVote,
+  getVoteById,
+  removeVote,
+  updateVote,
+};
